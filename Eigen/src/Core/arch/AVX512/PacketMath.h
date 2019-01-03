d6e4bd0e39ae42e806d2686f0181397e4275339b
Mark D Ryan <mark.d.ryan@intel.com>
2019-01-03 14:33:04 +0100

    PR560: Fix the AVX512f only builds
    Commit 22f9cc0 introduced AVX512 support for complex numbers but required
    avx512dq to build.  Commit ebab829 fixed some but not, it would seem all,
    of the hard avx512dq dependencies.  Build failures are still evident on
    Eigen and TensorFlow when compiling with just avx512f and no avx512dq
    using gcc 7.3.  Looking at the code there does indeed seem to be a problem.
    Commit 22f9cc0 calls avx512dq intrinsics directly, e.g, _mm512_extractf32x8_ps
    and _mm512_and_ps.  This commit fixes the issue by replacing the direct
    intrinsic calls with the various wrapper functions that are safe to use on
    avx512f only builds.

--- Eigen/src/Core/arch/AVX512/PacketMath.h
+++ Eigen/src/Core/arch/AVX512/PacketMath.h
@@ -262,7 +262,24 @@ EIGEN_STRONG_INLINE Packet8d pmax<Packet8d>(const Packet8d& a,
   return _mm512_max_pd(b, a);
 }
 
-template<> EIGEN_STRONG_INLINE Packet16i pcmp_eq(const Packet16i& a, const Packet16i& b) {
+#ifdef EIGEN_VECTORIZE_AVX512DQ
+template<int I> EIGEN_STRONG_INLINE Packet8f extract256(Packet16f x) { return _mm512_extractf32x8_ps(x,I); }
+template<int I> EIGEN_STRONG_INLINE Packet2d extract128(Packet8d x) { return _mm512_extractf64x2_pd(x,I); }
+EIGEN_STRONG_INLINE Packet16f cat256(Packet8f a, Packet8f b) { return _mm512_insertf32x8(_mm512_castps256_ps512(a),b,1); }
+#else
+// AVX512F does not define _mm512_extractf32x8_ps to extract _m256 from _m512
+template<int I> EIGEN_STRONG_INLINE Packet8f extract256(Packet16f x) {
+  return  _mm256_castsi256_ps(_mm512_extracti64x4_epi64( _mm512_castps_si512(x),I));
+}
+
+// AVX512F does not define _mm512_extractf64x2_pd to extract _m128 from _m512
+template<int I> EIGEN_STRONG_INLINE Packet2d extract128(Packet8d x) {
+  return _mm_castsi128_pd(_mm512_extracti32x4_epi32( _mm512_castpd_si512(x),I));
+}
+
+#endif
+
+ template<> EIGEN_STRONG_INLINE Packet16i pcmp_eq(const Packet16i& a, const Packet16i& b) {
   __m256i lo = _mm256_cmpeq_epi32(_mm512_extracti64x4_epi64(a, 0), _mm512_extracti64x4_epi64(b, 0));
   __m256i hi = _mm256_cmpeq_epi32(_mm512_extracti64x4_epi64(a, 1), _mm512_extracti64x4_epi64(b, 1));
   return _mm512_inserti64x4(_mm512_castsi256_si512(lo), hi, 1);
